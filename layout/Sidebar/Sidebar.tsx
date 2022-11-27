44ad128fcb5bf491964847a42a410381d66caed3
fwRelik <relik.fw@gmail.com>
2022-11-27 23:04:10 +0300

    Sidebar styles edit

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,9 +1,14 @@
 import { SidebarProps } from './Sidebar.props';
 import { Menu } from '../Menu/Menu';
+import Logo from '../logo.svg';
+import styles from './Sidebar.module.scss';
+import cn from 'classnames'
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
 	return (
-		<div {...props}>
+		<div {...props} className={cn(className, styles.sidebar)}>
+			<Logo className={styles.logo}/>
+			<div>Поиск</div>
 			<Menu />
 		</div>
 	);
