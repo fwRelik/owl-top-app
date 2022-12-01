b850afae04bd505796fd46a32eebcafb52c456de
fwRelik <relik.fw@gmail.com>
2022-12-01 03:44:02 +0300

    Validator helpers, global styles edit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { GetStaticProps } from 'next';
+import { Input } from '../components';
 import { MenuItem } from '../interfaces/menu.interface';
 import { withLayout } from '../layout/Layout';
 
@@ -9,6 +10,7 @@ function Home({ menu }: HomeProps): JSX.Element {
 			{menu.map(m => (
 				<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
 			))}
+			<Input placeholder={'Hello World!'}/>
 		</>
 	);
 }
