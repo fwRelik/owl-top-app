022e2ffdb5e0a8acfebf79398bd2a157d159750c
fwRelik <relik.fw@gmail.com>
2022-12-28 19:08:35 +0300

    Config file used

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { pageConfig } from '../configs';
+import { API, pageConfig } from '../configs';
 
 export default function App({ Component, pageProps, router }: AppProps): JSX.Element {
 	return (
 		<>
 			<Head>
 				<title>{pageConfig.title}</title>
-				<meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
-				<meta property='twitter:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+				<meta property='og:url' content={API.domain.host + router.asPath} />
+				<meta property='twitter:url' content={API.domain.host + router.asPath} />
 				<meta property='og:locale' content='ru_RU' />
 			</Head>
 			<Component {...pageProps} />
